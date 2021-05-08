99f87fb837659569d66049a86c29a66bc8a55792
Văn Toàn <hvtoan.dev@gmail.com>
2021-05-09 01:00:54 +0700

    update lesson 1.

---
+++ GiaoTrinhAndroidCoBan/Lesson1Demo/app/src/main/java/com/toandev/lesson1/data/DataClassJava.java
@@ -0,0 +1,28 @@
+package com.toandev.lesson1.data;
+
+/*
+Created by ToanDev on 02/05/2021
+Company: Netacom.
+Email: hvtoan.dev@gmail.com
+*/
+
+public class DataClassJava {
+    private String title;
+    private String author;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
